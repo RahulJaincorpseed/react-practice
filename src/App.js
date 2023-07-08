61cd48264d479299a4ffd395916e8c7289bec22e
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-08 10:30:15 +0530

    add react-toast in react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Toaster from './common/Toast/Toaster';
 
 function App() {
   return (
     <div className="App">
       <h1>Code Start...</h1>
+      <Routes>
+        <Route path="/toast" element={<Toaster />}/>
+      </Routes>
     </div>
   );
 }
