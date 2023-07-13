4174bcf158f38e13e63aa7dae303b3a3ef318a2e
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-13 12:27:37 +0530

    convert code in proper  indentation

--- src/BegineerMistake/StateMistakeThree.js
+++ src/BegineerMistake/StateMistakeThree.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 const StateMistakeThree = () => {
-    const [input, setInput] = useState("");
-    const [user, setUser] = useState({
-        name:"Rahul"
-    });
+  const [input, setInput] = useState("")
+  const [user, setUser] = useState({
+    name: "Rahul",
+  })
 
-    const changeName = () =>{
-        setUser((prev) => ({ ...prev , name : input}));
-    }   
+  const changeName = () => {
+    setUser((prev) => ({ ...prev, name: input }))
+  }
 
-    console.log("user is", user, input);
+  console.log("user is", user, input)
 
   return (
     <div>
-        <input onChange={(e)=> setInput(e.target.value)} type="text" />
-        <button onClick={changeName}>Change Name</button>
-        <h2>Username is {user?.name}</h2>
+      <input onChange={(e) => setInput(e.target.value)} type="text" />
+      <button onClick={changeName}>Change Name</button>
+      <h2>Username is {user?.name}</h2>
     </div>
   )
-};
+}
 
-export default StateMistakeThree;
+export default StateMistakeThree
